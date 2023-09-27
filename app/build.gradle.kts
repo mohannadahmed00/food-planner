033b31b5bdf7d44bfe58048458a3432a7052b751
mohannadahmed00 <mohannad199344@gmail.com>
2023-09-27 21:48:30 +0200

    navigation component has been handled

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("androidx.navigation.safeargs")
 }
 
 android {
@@ -39,4 +40,22 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //navigation
+    implementation("androidx.navigation:navigation-fragment:2.5.3")
+    implementation("androidx.navigation:navigation-ui:2.5.3")
+
+    //room
+    implementation("androidx.room:room-runtime:2.4.1")
+    annotationProcessor("androidx.room:room-compiler:2.4.1")
+
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //glide
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+
+    //circle imageview
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 }
--- app/src/main/java/com/giraffe/foodplannerapplication/MainActivity.java
+++ app/src/main/java/com/giraffe/foodplannerapplication/MainActivity.java
@@ -1,14 +1,43 @@
 package com.giraffe.foodplannerapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        navController.addOnDestinationChangedListener((navController, navDestination, bundle) -> {
+            if (navDestination.getId() == R.id.homeFragment || navDestination.getId() == R.id.planFragment || navDestination.getId() == R.id.favoritesFragment || navDestination.getId() == R.id.settingsFragment) {
+                showBottomNav();
+            } else {
+                hideBottomNav();
+            }
+        });
+    }
+
+    private void showBottomNav() {
+        bottomNavigationView.setVisibility(View.VISIBLE);
+
+    }
+
+    private void hideBottomNav() {
+        bottomNavigationView.setVisibility(View.GONE);
     }
 }
